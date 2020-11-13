89d1ab5338b9743f1d96be8cf8e4dac76d729df7
plpgamedev <plpgamedev@gmail.com>
2020-11-13 19:21:45 +0100

    OnScreen keyboard added & Word Bank update

--- src/main/resources/static/scenes/preLobby.js
+++ src/main/resources/static/scenes/preLobby.js
@@ -168,16 +168,41 @@ class PreLobby extends Phaser.Scene {
         //Background
         this.bg = this.add.image(0,0,'Menu').setInteractive();
 
-        //Buttons
+        //buttons
         this.button_create = this.add.image(0,0, 'Ready_es').setInteractive({cursor: 'pointer'});
-        this.button_join = this.add.image(0,0, 'Ready_host_es').setInteractive({cursor: 'pointer'});
         this.codeButton = this.add.image(0,0, 'Ready_host_es').setInteractive({cursor: 'pointer'});
+
+        //keyboard
+        this.keyBoardBg = this.add.image(0,0,'Gameplay').setInteractive();
+        this.BarraCod = this.add.image(0,0,'BarraCod').setInteractive();
+        this.ConfirmarCod = this.add.image(0,0,'ConfirmarCod').setInteractive({cursor: 'pointer'});
+        this.BorrarCod = this.add.image(0,0,'BorrarCod').setInteractive({cursor: 'pointer'});
+        this.SalirCod = this.add.image(0,0,'SalirCod').setInteractive({cursor: 'pointer'});
+        this.Letra_A = this.add.image(0,0,'Letra_A').setInteractive({cursor: 'pointer'});
+        this.Letra_B = this.add.image(0,0,'Letra_B').setInteractive({cursor: 'pointer'});
+        this.Letra_C = this.add.image(0,0,'Letra_C').setInteractive({cursor: 'pointer'});
+        this.Letra_D = this.add.image(0,0,'Letra_D').setInteractive({cursor: 'pointer'});
+        this.Letra_E = this.add.image(0,0,'Letra_E').setInteractive({cursor: 'pointer'});
+        this.Letra_F = this.add.image(0,0,'Letra_F').setInteractive({cursor: 'pointer'});
+        this.Letra_G = this.add.image(0,0,'Letra_G').setInteractive({cursor: 'pointer'});
+        this.Letra_H = this.add.image(0,0,'Letra_H').setInteractive({cursor: 'pointer'});
+        this.Letra_I = this.add.image(0,0,'Letra_I').setInteractive({cursor: 'pointer'});
+        this.Letra_J = this.add.image(0,0,'Letra_J').setInteractive({cursor: 'pointer'});
+        this.Letra_K = this.add.image(0,0,'Letra_K').setInteractive({cursor: 'pointer'});
+        this.Letra_L = this.add.image(0,0,'Letra_L').setInteractive({cursor: 'pointer'});
+        this.Letra_M = this.add.image(0,0,'Letra_M').setInteractive({cursor: 'pointer'});
+        this.Letra_N = this.add.image(0,0,'Letra_N').setInteractive({cursor: 'pointer'});
+        this.Letra_O = this.add.image(0,0,'Letra_O').setInteractive({cursor: 'pointer'});
+        this.Letra_P = this.add.image(0,0,'Letra_P').setInteractive({cursor: 'pointer'});
+
+        this.codeText = this.add.text(0, 0, '', { fontSize: '70px',color: '#000000',fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' });
+
+        this.invisible(false);
         this.scaler();
 
         this.input.keyboard.addKey(8);
         this.codeFocus = false;
     	
-        this.codeText = this.add.text(game.canvas.width * 3 / 4, game.canvas.height / 2, '', { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' });
 		this.button_create.on('pointerdown', function (pointer){
             let msg = new Object();
             msg.event = 'CREATE_ROOM';
@@ -188,18 +213,87 @@ class PreLobby extends Phaser.Scene {
         }, this);
         
         this.codeButton.on('pointerdown', function (pointer){
-            this.scene.codeFocus = true;
-            //console.log(this.scene);
-            this.codeButton.setTint(0x00ffff);
+            this.codeButton.setAlpha(0);
+            this.button_create.setAlpha(0);
+            this.scene.get('PreLobby').scene.codeFocus = true;
+            this.scene.get('PreLobby').invisible(true);
+            this.codeButton.removeInteractive();
+            this.button_create.removeInteractive();
 		}, this);
         this.bg.on('pointerdown', function (pointer){
-            this.scene.codeFocus = false;
-            this.codeButton.setTint(0xffffff);
+            this.codeButton.setAlpha(1);
+            this.button_create.setAlpha(1);
+            this.scene.get('PreLobby').scene.codeFocus = false;
+            this.scene.get('PreLobby').invisible(false);
+            this.codeButton.setInteractive({cursor: 'pointer'});
+            this.button_create.setInteractive({cursor: 'pointer'});
+        }, this);
+        this.SalirCod.on('pointerdown', function (pointer){
+            this.codeButton.setAlpha(1);
+            this.button_create.setAlpha(1);
+            this.scene.get('PreLobby').scene.codeFocus = false;
+            this.scene.get('PreLobby').invisible(false);
+            this.codeButton.setInteractive({cursor: 'pointer'});
+            this.button_create.setInteractive({cursor: 'pointer'});
 		}, this);
-		this.button_join.on('pointerdown', function (pointer){
+		this.ConfirmarCod.on('pointerdown', function (pointer){
             this.scene.get('PreLobby').tryJoin();
         }, this);
-        
+        this.BorrarCod.on('pointerdown', function (pointer){
+            if (!this.scene.get('PreLobby').scene.codeFocus) return;
+                    this.scene.get('PreLobby').trimText();
+        }, this);
+
+        this.Letra_A.on('pointerdown', function (pointer){
+            this.scene.get('PreLobby').updateText('A');
+        }, this);
+        this.Letra_B.on('pointerdown', function (pointer){
+            this.scene.get('PreLobby').updateText('B');
+        }, this);
+        this.Letra_C.on('pointerdown', function (pointer){
+            this.scene.get('PreLobby').updateText('C');
+        }, this);
+        this.Letra_D.on('pointerdown', function (pointer){
+            this.scene.get('PreLobby').updateText('D');
+        }, this);
+        this.Letra_E.on('pointerdown', function (pointer){
+            this.scene.get('PreLobby').updateText('E');
+        }, this);
+        this.Letra_F.on('pointerdown', function (pointer){
+            this.scene.get('PreLobby').updateText('F');
+        }, this);
+        this.Letra_G.on('pointerdown', function (pointer){
+            this.scene.get('PreLobby').updateText('G');
+        }, this);
+        this.Letra_H.on('pointerdown', function (pointer){
+            this.scene.get('PreLobby').updateText('H');
+        }, this);
+        this.Letra_I.on('pointerdown', function (pointer){
+            this.scene.get('PreLobby').updateText('I');
+        }, this);
+        this.Letra_J.on('pointerdown', function (pointer){
+            this.scene.get('PreLobby').updateText('J');
+        }, this);
+        this.Letra_K.on('pointerdown', function (pointer){
+            this.scene.get('PreLobby').updateText('K');
+        }, this);
+        this.Letra_L.on('pointerdown', function (pointer){
+            this.scene.get('PreLobby').updateText('L');
+        }, this);
+        this.Letra_M.on('pointerdown', function (pointer){
+            this.scene.get('PreLobby').updateText('M');
+        }, this);
+        this.Letra_N.on('pointerdown', function (pointer){
+            this.scene.get('PreLobby').updateText('N');
+        }, this);
+        this.Letra_O.on('pointerdown', function (pointer){
+            this.scene.get('PreLobby').updateText('O');
+        }, this);
+        this.Letra_P.on('pointerdown', function (pointer){
+            this.scene.get('PreLobby').updateText('P');
+        }, this);
+
+
 		this.input.keyboard.on('keydown', 
             function (event) { 
                 //console.log(this.scene.scene);
@@ -251,16 +345,38 @@ class PreLobby extends Phaser.Scene {
 		}
     }
 
+    invisible(isVisible)
+    {
+        this.codeText.setAlpha(isVisible)
+        this.keyBoardBg.setAlpha(isVisible);
+        this.BarraCod.setAlpha(isVisible);
+        this.ConfirmarCod.setAlpha(isVisible);
+        this.SalirCod.setAlpha(isVisible);
+        this.BorrarCod.setAlpha(isVisible);
+        this.Letra_A.setAlpha(isVisible);
+        this.Letra_B.setAlpha(isVisible);
+        this.Letra_C.setAlpha(isVisible);
+        this.Letra_D.setAlpha(isVisible);
+        this.Letra_E.setAlpha(isVisible);
+        this.Letra_F.setAlpha(isVisible);
+        this.Letra_G.setAlpha(isVisible);
+        this.Letra_H.setAlpha(isVisible);
+        this.Letra_I.setAlpha(isVisible);
+        this.Letra_J.setAlpha(isVisible);
+        this.Letra_K.setAlpha(isVisible);
+        this.Letra_L.setAlpha(isVisible);
+        this.Letra_M.setAlpha(isVisible);
+        this.Letra_N.setAlpha(isVisible);
+        this.Letra_O.setAlpha(isVisible);
+        this.Letra_P.setAlpha(isVisible);
+    }
+
     scaler()
     {
         //Buttons
         this.button_create.x = game.canvas.width / 4;
         this.button_create.y = game.canvas.height / 4;
         this.button_create.setScale(this.sY);
-
-        this.button_join.x = game.canvas.width * 3 / 4;
-        this.button_join.y = game.canvas.height / 4;
-        this.button_join.setScale(this.sY);
 
         this.codeButton.x = game.canvas.width / 4;
         this.codeButton.y = game.canvas.height / 2;
@@ -269,7 +385,106 @@ class PreLobby extends Phaser.Scene {
         //Background
         this.bg.x = game.canvas.width / 2;
 		this.bg.y = game.canvas.height / 2;
-		this.bg.setScale(this.sX);
+        this.bg.setScale(this.sX);
+        
+        this.keyBoardBg.x = game.canvas.width / 2;
+		this.keyBoardBg.y = game.canvas.height / 2;
+        this.keyBoardBg.setScale(this.sY*3/4);
+        
+        //---------------------KEYBOARD---------------------//
+        let kbLTCornerX = (this.keyBoardBg.x-(this.keyBoardBg.width*this.keyBoardBg.scaleX)/2)+0;
+        let kbLTCornerY = (this.keyBoardBg.y-(this.keyBoardBg.height*this.keyBoardBg.scaleY)/2)+0;
+        let columnPos = this.keyBoardBg.width*this.keyBoardBg.scaleX/20;
+        let rowPos = this.keyBoardBg.height*this.keyBoardBg.scaleY/6
+
+        //First Row
+        this.Letra_A.x = kbLTCornerX+columnPos*2;
+		this.Letra_A.y = kbLTCornerY+rowPos;
+        this.Letra_A.setScale(this.keyBoardBg.scale);
+
+        this.Letra_B.x = kbLTCornerX+columnPos*4;
+		this.Letra_B.y = kbLTCornerY+rowPos;
+        this.Letra_B.setScale(this.keyBoardBg.scale);
+        
+        this.Letra_C.x = kbLTCornerX+columnPos*6;
+		this.Letra_C.y = kbLTCornerY+rowPos;
+        this.Letra_C.setScale(this.keyBoardBg.scale);
+        
+        this.Letra_D.x = kbLTCornerX+columnPos*8;
+		this.Letra_D.y = kbLTCornerY+rowPos;
+        this.Letra_D.setScale(this.keyBoardBg.scale);
+        
+        this.Letra_E.x = kbLTCornerX+columnPos*10;
+		this.Letra_E.y = kbLTCornerY+rowPos;
+        this.Letra_E.setScale(this.keyBoardBg.scale);
+        
+        this.Letra_F.x = kbLTCornerX+columnPos*12;
+		this.Letra_F.y = kbLTCornerY+rowPos;
+        this.Letra_F.setScale(this.keyBoardBg.scale);
+        
+        this.Letra_G.x = kbLTCornerX+columnPos*14;
+		this.Letra_G.y = kbLTCornerY+rowPos;
+        this.Letra_G.setScale(this.keyBoardBg.scale);
+        
+        this.Letra_H.x = kbLTCornerX+columnPos*16;
+		this.Letra_H.y = kbLTCornerY+rowPos;
+        this.Letra_H.setScale(this.keyBoardBg.scale);
+        
+        //Second Row
+        this.Letra_I.x = kbLTCornerX+columnPos*2;
+		this.Letra_I.y = kbLTCornerY+rowPos*2;
+        this.Letra_I.setScale(this.keyBoardBg.scale);
+        
+        this.Letra_J.x = kbLTCornerX+columnPos*4;
+		this.Letra_J.y = kbLTCornerY+rowPos*2;
+        this.Letra_J.setScale(this.keyBoardBg.scale);
+        
+        this.Letra_K.x = kbLTCornerX+columnPos*6;
+		this.Letra_K.y = kbLTCornerY+rowPos*2;
+        this.Letra_K.setScale(this.keyBoardBg.scale);
+        
+        this.Letra_L.x = kbLTCornerX+columnPos*8;
+		this.Letra_L.y = kbLTCornerY+rowPos*2;
+        this.Letra_L.setScale(this.keyBoardBg.scale);
+        
+        this.Letra_M.x = kbLTCornerX+columnPos*10;
+		this.Letra_M.y = kbLTCornerY+rowPos*2;
+        this.Letra_M.setScale(this.keyBoardBg.scale);
+        
+        this.Letra_N.x = kbLTCornerX+columnPos*12;
+		this.Letra_N.y = kbLTCornerY+rowPos*2;
+        this.Letra_N.setScale(this.keyBoardBg.scale);
+        
+        this.Letra_O.x = kbLTCornerX+columnPos*14;
+		this.Letra_O.y = kbLTCornerY+rowPos*2;
+        this.Letra_O.setScale(this.keyBoardBg.scale);
+        
+        this.Letra_P.x = kbLTCornerX+columnPos*16;
+		this.Letra_P.y = kbLTCornerY+rowPos*2;
+        this.Letra_P.setScale(this.keyBoardBg.scale);
+        
+        //Confirmation row
+        this.BarraCod.x = kbLTCornerX+columnPos*7;
+        this.BarraCod.y = kbLTCornerY+rowPos*4
+        this.BarraCod.setScale(this.keyBoardBg.scale);
+        
+        this.BorrarCod.x = kbLTCornerX+columnPos*7;
+        this.BorrarCod.y = kbLTCornerY+rowPos*5
+        this.BorrarCod.setScale(this.keyBoardBg.scale);
+
+        this.ConfirmarCod.x = kbLTCornerX+columnPos*7+((this.BarraCod.width*this.BarraCod.scaleX)/2)*1.5;
+        this.ConfirmarCod.y = kbLTCornerY+rowPos*4
+        this.ConfirmarCod.setScale(this.keyBoardBg.scale);
+
+        this.SalirCod.x = kbLTCornerX+columnPos*18;
+        this.SalirCod.y = kbLTCornerY+rowPos/2
+        this.SalirCod.setScale(this.keyBoardBg.scale);
+
+        this.codeText.x = this.BarraCod.x-((this.BarraCod.width*this.BarraCod.scaleX)/2)*0.8;
+        this.codeText.y = this.BarraCod.y*0.95;
+        this.codeText.scaleX = this.BarraCod.scaleX;
+        this.codeText.scaleY = this.BarraCod.scaleY;
+
     }
 
     joinRoom(roomCode)
--- src/main/resources/static/scenes/preloader.js
+++ src/main/resources/static/scenes/preloader.js
@@ -8,7 +8,12 @@ class Preloader extends Phaser.Scene {
     }
     preload() {
     	this.load.image('Menu', 'assets/backgrounds/Menu.png');
-    	this.load.image('Gameplay', 'assets/backgrounds/Gameplay.png');
+    	this.load.image('KeyBoardBg', 'assets/backgrounds/KeyBoardBg.png');
+		this.load.image('ConfirmarCod', 'assets/backgrounds/Confirmar.png');
+    	this.load.image('SalirCod', 'assets/backgrounds/Salir.png');
+    	this.load.image('BarraCod', 'assets/backgrounds/BarraCodigo.png');
+    	this.load.image('BorrarCod', 'assets/backgrounds/Borrar.png');
+		this.load.image('Gameplay', 'assets/backgrounds/Gameplay.png');
     	this.load.image('Caballete_gameplay', 'assets/interface/Caballete_gameplay.png');
     	this.load.image('Ready_es', 'assets/interface/Ready_es.png');
     	this.load.image('Ready_en', 'assets/interface/Ready_en.png');
@@ -20,7 +25,24 @@ class Preloader extends Phaser.Scene {
     	this.load.image('Marco1', 'assets/interface/Marco1.png');
     	this.load.image('Marco2', 'assets/interface/Marco2.png');
     	this.load.image('Marco3', 'assets/interface/Marco3.png');
-    	this.load.image('Marco4', 'assets/interface/Marco4.png');
+		this.load.image('Marco4', 'assets/interface/Marco4.png');
+		//letras teclado
+    	this.load.image('Letra_A', 'assets/backgrounds/letras/A.png');
+    	this.load.image('Letra_B', 'assets/backgrounds/letras/B.png');
+    	this.load.image('Letra_C', 'assets/backgrounds/letras/C.png');
+    	this.load.image('Letra_D', 'assets/backgrounds/letras/D.png');
+    	this.load.image('Letra_E', 'assets/backgrounds/letras/E.png');
+    	this.load.image('Letra_F', 'assets/backgrounds/letras/F.png');
+    	this.load.image('Letra_G', 'assets/backgrounds/letras/G.png');
+    	this.load.image('Letra_H', 'assets/backgrounds/letras/H.png');
+    	this.load.image('Letra_I', 'assets/backgrounds/letras/I.png');
+    	this.load.image('Letra_J', 'assets/backgrounds/letras/J.png');
+    	this.load.image('Letra_K', 'assets/backgrounds/letras/K.png');
+    	this.load.image('Letra_L', 'assets/backgrounds/letras/L.png');
+    	this.load.image('Letra_M', 'assets/backgrounds/letras/M.png');
+    	this.load.image('Letra_N', 'assets/backgrounds/letras/N.png');
+    	this.load.image('Letra_O', 'assets/backgrounds/letras/O.png');
+		this.load.image('Letra_P', 'assets/backgrounds/letras/P.png');
     }
 
     create() {
